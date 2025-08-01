ba827607c56af07e2b6d168d31acdf960c402179
lji <571250422@qq.com>
2025-08-02 00:32:20 +0800

    add prompts, remove inappropriate settings in gitignore

--- verl/eval_agent/eval.sh
+++ verl/eval_agent/eval.sh
@@ -3,4 +3,3 @@ python -m eval_agent.main --agent_config er1_alfworld --exp_config alfworld_v2 -
 
 python -m eval_agent.main --agent_config er1_sciworld --exp_config sciworld_v3 --split dev --verbose
 python -m eval_agent.main --agent_config er1_sciworld --exp_config sciworld_v3 --split test --verbose
-# /inspire/hdd/project/embodied-multimodality/qiuxipeng-24028/xpqiu/lji/data/Qwen/Qwen2.5-0.5B-Instruct
